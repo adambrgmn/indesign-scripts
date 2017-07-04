47ad323ddfeef84d578140a184eb6ef2194d8f34
Adam Bergman <adam@fransvilhelm.com>
2017-07-04 13:58:50 +0200

    :art: Update notify to use InDesigns built in dialog instead of alert

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,2 +1,8 @@
-/* eslint-disable no-alert */
-export default msg => alert(msg);
+export default (msg, title = '') => {
+  const dialog = app.dialogs.add({ name: title, canCancel: false });
+  const column = dialog.dialogColumns.add();
+  const row = column.dialogRows.add();
+  row.staticTexts.add({ staticLabel: msg });
+
+  dialog.show();
+};
