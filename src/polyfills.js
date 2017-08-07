37394e5b54532f54ec9511030477d357423dfdd6
Adam Bergman <adam@fransvilhelm.com>
2017-08-07 16:15:29 +0200

    :art: Add forEach polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -203,6 +203,41 @@ if (!Array.prototype.map) {
   };
 }
 
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function forEach(callback, thisArg = void 0) {
+    let T;
+    let k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    const O = Object(this);
+    const len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+      throw new TypeError(`${callback} is not a function`);
+    }
+
+    if (thisArg) {
+      T = thisArg;
+    }
+
+    k = 0;
+
+    while (k < len) {
+      let kValue;
+
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+
+      k += 1;
+    }
+  };
+}
+
 if (!Object.keys) {
   Object.keys = (function keys() {
     const hasOwnProperty = Object.prototype.hasOwnProperty;
