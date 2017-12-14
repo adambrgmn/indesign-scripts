7e71f94c3b265fffd60d8b1932c4c9b450a38359
Adam Bergman <adam@fransvilhelm.com>
2017-12-14 16:14:09 +0100

    ci: Update CI settings

--- scripts/travis-after-success.js
+++ scripts/travis-after-success.js
@@ -1,43 +1,4 @@
-const path = require('path');
 const spawn = require('cross-spawn'); // eslint-disable-line
-
-function getConcurrentlyArgs(scripts, { killOthers = true } = {}) {
-  const colors = [
-    'bgBlue',
-    'bgGreen',
-    'bgMagenta',
-    'bgCyan',
-    'bgWhite',
-    'bgRed',
-    'bgBlack',
-    'bgYellow',
-  ];
-
-  // eslint-disable-next-line
-  scripts = Object.entries(scripts).reduce((all, [name, script]) => {
-    if (script) {
-      return Object.assign({}, all, { [name]: script });
-    }
-    return all;
-  }, {});
-
-  const prefixColors = Object.keys(scripts)
-    .reduce(
-      (pColors, _s, i) =>
-        pColors.concat([`${colors[i % colors.length]}.bold.reset`]),
-      [],
-    )
-    .join(',');
-
-  // prettier-ignore
-  return [
-    killOthers ? '--kill-others-on-fail' : null,
-    '--prefix', '[{name}]',
-    '--names', Object.keys(scripts).join(','),
-    '--prefix-colors', prefixColors,
-    ...Object.values(scripts).map(s => JSON.stringify(s)), // stringify escapes quotes ✨
-  ].filter(Boolean)
-}
 
 function envIsSet(name) {
   return (
@@ -65,18 +26,7 @@ if (!autorelease) {
     'No need to autorelease or report coverage. Skipping travis-after-success script...',
   );
 } else {
-  const result = spawn.sync(
-    path.join(__dirname, '..', 'node_modules/.bin', 'concurrently'),
-    getConcurrentlyArgs(
-      {
-        release: autorelease
-          ? `echo installing semantic-release && npx -p semantic-release@8 -c 'echo running semantic-release && semantic-release pre && semantic-release post'`
-          : null,
-      },
-      { killOthers: false },
-    ),
-    { stdio: 'inherit' },
-  );
+  const result = spawn.sync('yarn', ['run', 'semantic-release']);
 
   process.exit(result.status);
 }
