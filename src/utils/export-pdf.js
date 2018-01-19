5352bf418f8654432291afa5086666bf96e1dada
Adam Bergman <adam@fransvilhelm.com>
2018-01-19 09:24:18 +0100

    fix(export-pdf): Create new param to export pdf to define pageRange
    fix #10

--- src/utils/export-pdf.js
+++ src/utils/export-pdf.js
@@ -1,4 +1,9 @@
-function exportPdf(doc, filename, { preset, backgroundJob = true } = {}) {
+function exportPdf(
+  doc,
+  filename,
+  { preset, backgroundJob = true, pageRange = PageRange.ALL_PAGES } = {},
+) {
+  app.pdfExportPreferences.pageRange = pageRange;
   const args = [ExportFormat.PDF_TYPE, new File(filename), false, preset];
 
   if (backgroundJob) return doc.asynchronousExportFile(...args);
